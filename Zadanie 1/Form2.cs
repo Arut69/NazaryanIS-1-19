70c0a88016d83a43bb480cdc961f02e48c376696
Asuma Hach <nazaryan.arut18@gmail.com>
2021-12-06 18:35:48 +0500

    Задание 2

---
+++ Zadanie 1/Form2.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+            static class BazaDanyh
+            {
+            public static string host = "caseum.ru";
+            public static string port = "33333";
+            public static string user_id = "test_user";
+            public static string database = "db_test";
+            public static string password = "test_pass";
+
+            public static string sqlconn()
+            {
+                string conn = $"server={host};port={port};user={user_id};database={database};password={password}";
+                return conn;
+            }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conec = new MySqlConnection(BazaDanyh.sqlconn());
+            try
+            {
+                conec.Open();
+                MessageBox.Show("База данных подключена");
+            }
+            catch
+            {
+                conec.Close();
+            }
+        }
+    }
+}
+
---
+++ Zadanie 1/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_1
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 Form1 = new Form1();
+            Form1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 Form2 = new Form2();
+            Form2.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
